eb162c1329c6bcc9e7faaba9fe46b9c2a9b8ceea
leimingming <1358079298@qq.com>
2019-08-30 16:48:25 +0800

    完成版

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,11 @@
 <template>
     <li @mouseenter="handlerEnter(true)" @mouseleave="handlerEnter(false)" :class="{editing:currentTodo}">
         <div class="view">
-            <a-checkbox v-model="todo.complete" style="padding:0px 0px 10px 10px "></a-checkbox>
+            <a-checkbox :checked="todo.complete" style="padding:0px 0px 10px 10px " @change="changeTodo"></a-checkbox>
             <label class="todo-content" :class="{'completeLable':todo.complete}" @dblclick="toEdit(todo.content)">{{todo.content}}</label>
             <a-icon type="close" v-show="isShow" @click="deleteTodo" style="color:#cc9a9a;float: right;font-size: 18px;padding: 13px 20px 0 0;"></a-icon>
         </div>
-        <a-input v-show="currentTodo" :value="todo.content" size="large" @keyup.enter="saveTodo"  @keyup.esc="cancelEdit"  @blur="saveTodo" v-model="currentTodo"></a-input>
+        <a-input v-show="currentTodo" :value="todo.content" size="large" @keyup.enter="$event.target.blur"  @keyup.esc="cancelEdit"  @blur="saveTodo" v-model="currentTodo"></a-input>
     </li>
 </template>
 
@@ -42,9 +42,12 @@
                     index: index,
                     currentTodo: currentTodo
                 }
-
                 this.$store.dispatch('saveTodo', newTodo)
                 this.currentTodo = null
+
+            },
+            changeTodo (obj) {
+                this.todo.complete = obj.target.checked
             }
 
         }
@@ -53,7 +56,8 @@
 
 <style>
 .completeLable{
-    text-decoration: line-through solid #EEEEE0;
+    text-decoration: line-through solid #d9d9d9;
+    color: #d9d9d9;
 }
 .editing > .view{
     display: none;
